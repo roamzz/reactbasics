3686f29f70d13d49bebd91c02e0c16ce0b26678b
Irini Pantopoulou <ipantopoulou@draxis.gr>
2021-09-28 18:52:57 +0300

    components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+
+
+
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">£294.67</div>
+        <h2>{props.title}  h2</h2>
+        <div className="expense-item__price">£{props.amount} amount</div>
       </div>
     </div>
   );
