13339799e7d3f2ceb040e4c5488eac2ae54453be
Irini Pantopoulou <ipantopoulou@draxis.gr>
2021-09-30 20:30:12 +0300

    new Expense Form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Expenses from './components/Expenses/Expenses';
-
+import NewExpense from './components/NewExpense/NewExpense';
 function App() {
 const expenses = [
     {
@@ -24,7 +24,7 @@ const expenses = [
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
 
       <Expenses items={expenses}></Expenses>
 
