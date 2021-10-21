be8b2c4d34b7a8bd9eeef1f0b75c99f2b5e1eb59
Irini Pantopoulou <ipantopoulou@draxis.gr>
2021-10-21 17:46:15 +0300

    more component list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
 import ExpenseFilter from './ExpensesFilters/ExpensesFilter';
 import Card from '../UI/Card';
 import './Expenses.css';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -16,41 +16,17 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  //you can even create the content here and pass the variable down to the expression
-  // let expenseContent = <p>No results found!</p>;
-
-//we can even check here and down in jsx there we just apply only the expenseContent variable
-//  if (filteredExpenses.length > 0) {
-//    expenseContent = filteredExpenses.map((expense) => (
-//      <ExpenseItem
-//        title={expense.title}
-//        amount={expense.amount}
-//        date={expense.date}
-//        key={expense.id}
-//      />
-//    ));
-//  }
-
-
   return (
     <div>
-      <Card className='expenses'>
-        <ExpenseFilter
-          selected={filteredYear}
-          onChangeFilter={filterChangeHandler}
-        />
-
-        {filteredExpenses.length === 0 && <p>No results found!</p>}
-        {filteredExpenses.length > 0 &&
-          filteredExpenses.map((expense) => (
-            <ExpenseItem
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-              key={expense.id}
-            />
-          ))}
-      </Card>
+      <list>
+        <Card className='expenses'>
+          <ExpenseFilter
+            selected={filteredYear}
+            onChangeFilter={filterChangeHandler}
+          />
+          <ExpensesList items={filteredExpenses} />
+        </Card>
+      </list>
     </div>
   );
 };
